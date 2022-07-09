88a3621bb275fd64f00d829f1623425b2f6b3cd6
Ghazawi vip <92712096+Ghazawi@users.noreply.github.com>
2022-07-09 04:47:01 +0300

    Update update.sh

--- update.sh
+++ update.sh
@@ -2,15 +2,15 @@
 clear
 
 sudo chmod +x /etc/
-
+chmod +x /etc/
 clear
 
 sudo chmod +x /usr/share/doc
-
+chmod +x /usr/share/d
 clear
 
 sudo rm -rf /usr/share/doc/amino/
-
+ rm -rf /usr/share/doc/amino
 clear
 
 cd /etc/
@@ -18,7 +18,7 @@ cd /etc/
 clear
 
 sudo rm -rf /etc/atacamino
-
+rm -rf /etc/atacamino
 clear
 
 mkdir atacamino
