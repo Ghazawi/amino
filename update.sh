78064b54edae5e5c2dfa9b83ef6dfc1ef112833a
Ghazawi vip <92712096+Ghazawi@users.noreply.github.com>
2022-07-09 07:28:43 +0300

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,31 +1,6 @@
 
 
 
-sudo chmod +x /etc/
-
-
-
-sudo chmod +x /usr/share/doc
-
-
-
-sudo rm -rf /usr/share/doc/amino/
-
-
-
-cd /etc/
-
-
-
-sudo rm -rf /etc/atacamino
-
-
-
-mkdir atacamino
-
-
-
-cd atacamino
 
 
 git clone https://github.com/Ghazawi/amino.git
