870335e09d1a5bd22ed1c45cab0f27c0fe56a364
Ghazawi vip <92712096+Ghazawi@users.noreply.github.com>
2022-07-13 01:03:11 +0300

    Update amino.sh

--- amino.sh
+++ amino.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+
 # By dev GHAZAWIVIP
-#-----------------------------------
-#sudo snap install jq #----install--
-#-----------------------------------
+#------------install-----------------
+#sudo snap install jq              #|
+#sudo apt-get install wget         #|
+#------------------------------------
+bash update.sh
 clear
 
 BLACK='\e[30m'
@@ -22,30 +25,97 @@ echo -e "${YELLOW} Telegram|@invietamino|Invitation ${NC} "
 echo ""
 echo "------------------------------------"
 echo ""
-echo -e "${RED}[!] This Tool Must Run As ROOT [!] ${NC}"
-echo ""
-echo -e "${CYAN}[>] Press ENTER to Install amino, CTRL+C to Abort.${NC}"
-read INPUT
-echo ""
+echo -e "${RED}[!] First version 1.0[!] ${NC}"
+echo "-"
+echo -e "${CYAN}[>] Always update the tool from a file update.sh ${NC}"
+echo "-"
 
 echo -e "${RED} [1] ${NC}  ${CYAN} id amino     ${NC}     "  
 echo -e "${RED} [2] ${NC}  ${CYAN} Voice calls  ${NC}     "  
-read -p  "        " NU
+read -p "$(echo -e $RED" imput "$NC)" NU
+if [ $NU == 1 ]; then
  echo -e  "\033[47mGet id amino By GAZAWIVIP-sors\033[m" 
-URL_ID_API="https://dev-sors.pantheonsite.io/api/v1/link-resolution.php?q="
-read -p  "add link profile amino:" PROFILEID
-if [[ $PROFILEID == *"http://aminoapps.com/"* ]];  then
-URL_PROFILE="$PROFILEID"
-NDCID=$(wget -qO-    "$URL_ID_API$URL_PROFILE"|jq '.ndcId'   )
-OBJECTID=$(wget -qO- "$URL_ID_API$URL_PROFILE"|jq '.objectId')
-NICKNAME=$(wget -qO- "$URL_ID_API$URL_PROFILE"|jq '.nickname')
+ NDCDEVICEID="42CBBA8E3D2DE693F7713F10C0587F29AEB3415C7558B75A96C43B14D0F41846C23128438CE4D6D597"
+ URL_ID_API="https://service.narvii.com/api/v1/g/s/link-resolution?q="
+read  -p  "$(echo -e ${YELLOW}"input link profile amino:"$NC) " URLPROFILEID
+fi
+if [[ $URLPROFILEID == *"http://aminoapps.com/"* ]];  then
+NDCDEVICEID="42CBBA8E3D2DE693F7713F10C0587F29AEB3415C7558B75A96C43B14D0F41846C23128438CE4D6D597"
+MESSAGEURLPROFILEID=$(curl --request GET "${URL_ID_API}${URLPROFILEID}" \
+--silent \
+--header "NDCDEVICEID:$NDCDEVICEID" \ | jq '{"api:message"}' )
+#-----------------GET VALUES ID AMINO---------------------------------------------------
+#            -----------GHAZAWI VIP-----------                     |                   #|
+#-----------------GET objectId -------------------------------------                   #|
+#         linkInfoV2.extensions.linkInfo.objectId                  |                   #|
+#-------------------------------------------------------------------                   #|
+OBJECTID=$(curl --request GET "${URL_ID_API}${URLPROFILEID}" \
+--silent \
+--header "NDCDEVICEID:$NDCDEVICEID" \ | jq '.linkInfoV2.extensions.linkInfo.objectId') #| 
+#-----------------GET objectId -------------------------------------                   #|
+#         linkInfoV2.extensions.linkInfo.ndcId                     |                   #|
+#-------------------------------------------------------------------                   #|
+NDCID=$(curl --request GET "${URL_ID_API}${URLPROFILEID}" \
+--silent \
+--header "NDCDEVICEID:$NDCDEVICEID" \ | jq ".linkInfoV2.extensions.linkInfo.ndcId" )   #|
 
- echo -e  "\033[47mnickname\033[m" "\033[41m$NICKNAME\033[m"
- echo   ""
+#---------------------------------------------------------------------------------------
+
+ echo   " "
  echo -e  "\033[47mndcId\033[m   " " \033[41m$NDCID\033[m  "
- echo   ""
+ echo   " "
  echo -e  "\033[47mobjectId\033[m" "\033[41m$OBJECTID\033[m"
- echo   ""
-else
-   echo -e " ${RED} Not error url ${NC} "
+ fi
+ 
+ if [ $NU == 2 ];then
+ echo  -e  "\033[47m login amino By GAZAWIVIP-sors\033[m" 
+ read  -p  "$(echo -e ${YELLOW}"input sid amino login:"$NC) " SID
+ read  -p  "$(echo -e ${YELLOW}"input link chat host:"$NC) " CHAT
+ read  -p  "$(echo -e ${YELLOW}"input link profile amino:"$NC) " URLPROFILEIDHOST
+
+if [[ ${URLPROFILEIDHOST} == *"http://aminoapps.com/"* && ${CHAT} == *"http://aminoapps.com/"* && ${SID} == *"sid="* ]];  then
+NDCDEVICEID="42CBBA8E3D2DE693F7713F10C0587F29AEB3415C7558B75A96C43B14D0F41846C23128438CE4D6D597"
+ URL_ID_API="https://service.narvii.com/api/v1/g/s/link-resolution?q="
+
+NDCID=$(curl --request GET "${URL_ID_API}${CHAT}" \
+--silent \
+--header "NDCDEVICEID:$NDCDEVICEID" \ | jq ".linkInfoV2.extensions.linkInfo.ndcId")
+
+OBJECTID2=$(curl --request GET "${URL_ID_API}${CHAT}" \
+--silent \
+--header "NDCDEVICEID:$NDCDEVICEID" \ | jq ".linkInfoV2.extensions.linkInfo.objectId")
+
+MESSAGE_chat=$(wget  -qO- "${URL_ID_API}${CHAT}" |jq '{"api:message"}' )
+
+OBJECTID=$(curl --request GET "${URL_ID_API}${URLPROFILEIDHOST}" \
+--silent \
+--header "NDCDEVICEID:$NDCDEVICEID" \ | jq  ".linkInfoV2.extensions.linkInfo.objectId" )
+
+
+OBJECT_ID=`echo $OBJECTID  | tr -d '"'`
+OBJECT_ID2=`echo $OBJECTID2  | tr -d '"'`
+
+sleep 2
+service="https://service.narvii.com/api/v1/x${NDCID}/s/chat/thread/${OBJECT_ID2}/co-host/${OBJECT_ID}"
+
+COHOST=$(curl --request DELETE \
+--silent \
+--header "NDCAUTH:${SID}" \
+--header "NDCDEVICEID:$NDCDEVICEID" \
+$service | jq '{"api:message"}')
+echo $COHOST
+if [[ ${COHOST} == *"OK"* ]];  then
+for (( ; ; ))
+do
+COHOST=$(curl --request DELETE \
+--silent \
+--header "NDCAUTH:${SID}" \
+--header "NDCDEVICEID:${NDCDEVICEID}" \
+$service | jq '{"api:message"}')
+  echo -e "[${YELLOW}OK${NC}] ${COHOST} "
+done
+ else 
+ echo "${RED}$COHOST${RED}"
+fi
+fi
 fi
